f39987feed7c2a787142f44caab9ad08418fbe81
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-06-17 18:23:58 +0100

    Update copyright

--- cmmlinflam/stem.py
+++ cmmlinflam/stem.py
@@ -1,8 +1,8 @@
 #
-# PheSEIRModel Class
+# StemGillespie Class
 #
-# This file is part of EPIMODELS
-# (https://github.com/I-Bouros/multi-epi-model-cross-analysis.git) which is
+# This file is part of CMMLINFLAM
+# (https://github.com/I-Bouros/cmml-inflam.git) which is
 # released under the MIT license. See accompanying LICENSE for copyright
 # notice and full license details.
 #
---
+++ cmmlinflam/tests/test_stem.py
@@ -0,0 +1,21 @@
+#
+# This file is part of CMMLINFLAM
+# (https://github.com/I-Bouros/cmml-inflam.git) which is
+# released under the MIT license. See accompanying LICENSE for copyright
+# notice and full license details.
+#
+
+import unittest
+
+import numpy as np
+import numpy.testing as npt
+
+import cmmlinflam as ci
+
+
+class TestPheSEIRModel(unittest.TestCase):
+    """
+    Test the 'PheSEIRModel' class.
+    """
+    def test__init__(self):
+        pass
